0a1f15bed1aeee2648becc01adc506b72acfe7ee
maheshmc <mahesh.cm@gmail.com>
2022-12-02 18:19:01 +0530

    7
    7

--- src/main/java/io/swagger/Swagger2SpringBoot.java
+++ src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -6,10 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -27,7 +28,7 @@ public class Swagger2SpringBoot implements CommandLineRunner {
             filter.setIncludeQueryString(true);
             filter.setIncludePayload(true);
             filter.setMaxPayloadLength(10000);
-            filter.setIncludeHeaders(false);
+            filter.setIncludeHeaders(true);
             filter.setAfterMessagePrefix("REQUEST DATA : ");
             return filter;
         }
